d137411e1a744416e2f9d97d46188a3241577447
dev_Scott <chengscott@live.com>
2016-02-15 10:02:22 +0800

    add blog

---
+++ blog/js/pdf.js
@@ -0,0 +1,4 @@
+function loadPDF(id) {
+	var docViewerSrc = "https://docs.google.com/viewer?srcid=DOCID&pid=explorer&efh=false&a=v&chrome=false&embedded=true";
+	$("#pdf").attr("src", docViewerSrc.replace('DOCID', id));
+}
