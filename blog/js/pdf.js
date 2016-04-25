8be1e6d996da9f66d5c401407c5af18ecf01faad
dev_Scott <chengscott@live.com>
2016-04-25 08:30:10 +0800

    fix indention

--- blog/js/pdf.js
+++ blog/js/pdf.js
@@ -1,4 +1,5 @@
 function loadPDF(id) {
-	var docViewerSrc = "https://docs.google.com/viewer?srcid=DOCID&pid=explorer&efh=false&a=v&chrome=false&embedded=true";
-	$("#pdf").attr("src", docViewerSrc.replace('DOCID', id));
+    var docViewerSrc = 'https://docs.google.com/viewer' +
+        '?srcid=DOCID&pid=explorer&efh=false&a=v&chrome=false&embedded=true';
+    $('#pdf').attr('src', docViewerSrc.replace('DOCID', id));
 }
