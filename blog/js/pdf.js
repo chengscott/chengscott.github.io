721c408990bf6f29af9549c10213b7e17de6e1de
chengscott <60510scott@gmail.com>
2017-07-14 00:57:41 +0800

    delete blog

--- blog/js/pdf.js
+++
@@ -1,5 +0,0 @@
-function loadPDF(id) {
-    var docViewerSrc = 'https://docs.google.com/viewer' +
-        '?srcid=DOCID&pid=explorer&efh=false&a=v&chrome=false&embedded=true';
-    $('#pdf').attr('src', docViewerSrc.replace('DOCID', id));
-}
